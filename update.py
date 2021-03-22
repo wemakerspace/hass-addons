e47cfa3454ed31ca4ace49701b340a12119ce590
Benjamin George Roberts <benjamin@bgroberts.id.au>
2021-03-23 08:14:29 +1100

    wip auto-update script

---
+++ update.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+import requests
+import json
+import re
+import subprocess
+from distutils.version import LooseVersion
+
+def call_all_or_fail(*cmds):
+    for cmd in cmds:
+        retcode = subprocess.call(cmd)
+        if retcode != 0:
+            cmd_str = "".join(cmd)
+            raise RuntimeError(f"non-zero ret code {retcode} returned for command {cmd_str}")
+        
+def update_tailscale():
+    config_path = "tailscale/config.json"
+    build_path = "tailscale/build.json"
+    # read current config
+    with open(config_path, "r") as f:
+        config = json.loads(f.read())
+    
+    with open(build_path, "r") as f:
+        build = json.loads(f.read())
+
+    local_version = LooseVersion(config['version'])
+    print(f"current repo version: {local_version}")
+    
+    # Get the current requests page
+    r = requests.get("https://pkgs.tailscale.com/stable/")
+    r.raise_for_status()
+
+    # version regex: 
+    version_regex = re.compile('href\\w*=\\w*"tailscale_((?:\\d+\\.?)+)_[\\d\\w]+\\.tgz"')
+    version_matches = set(version_regex.findall(r.text))
+
+    try:
+        remote_version = LooseVersion(version_matches.pop())
+    except KeyError:
+        raise RuntimeError("regex didn't match any versions on tailscale stable release page")
+
+    if len(version_matches) != 0:
+        raise RuntimeError("regex was ambiguous matching tailscale version")
+    
+    print(f"current upstream version: {remote_version}")
+
+    if remote_version > local_version:
+        # set build version 
+        build["args"]["TAILSCALE_VERSION"] = remote_version
+
+        with open(build_path, "w") as f:
+            f.write(json.dumps(build, indent=2))
+
+        # Pad new config version string to 4 parts
+        new_version = str(remote_version)
+        new_version += ".0" * (3 - new_version.count("."))
+
+        print(f"upgrade required, new version: {new_version}")
+        config['version'] = new_version
+        with open(config_path, "w") as f:
+            f.write(json.dumps(config, indent=2))
+        
+        # git add/commit/push
+        call_all_or_fail(
+            ["git", "add", config_path],
+            ["git" "add", build_path],
+            ["git", "commit", "-m", f"updated tailscale to {new_version}"]
+        )
+
+        return (local_version, new_version)
+        
+    else:
+        print("no upgrade required")
+        return None
+
+
+if __name__ == "__main__":
+    # git setup
+    call_all_or_fail(
+        ["git", "config", "--local", "user.name", "update.py"],
+        ["git", "config", "--local", "user.email", "update.py@hass-addons.git"]
+    )
+
+    ts_ver_tpl = update_tailscale()
+
+    # TODO email notify/push
+
