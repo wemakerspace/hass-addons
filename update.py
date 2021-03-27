dd7b806ef5d6bcf851ccb18abf8a745182c6a773
Benjamin George Roberts <benjamin@bgroberts.id.au>
2021-03-27 11:26:54 +1100

    update.py updates docker version

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 import requests
 import json
 import re
@@ -8,6 +8,7 @@ from distutils.version import LooseVersion
 def call_all_or_fail(*cmds):
     for cmd in cmds:
         retcode = subprocess.call(cmd)
+        
         if retcode != 0:
             cmd_str = "".join(cmd)
             raise RuntimeError(f"non-zero ret code {retcode} returned for command {cmd_str}")
@@ -15,12 +16,17 @@ def call_all_or_fail(*cmds):
 def update_tailscale():
     config_path = "tailscale/config.json"
     build_path = "tailscale/build.json"
+    docker_path = "tailscale/Dockerfile"
+
     # read current config
     with open(config_path, "r") as f:
         config = json.loads(f.read())
     
     with open(build_path, "r") as f:
         build = json.loads(f.read())
+
+    with open(docker_path, "r") as f:
+        docker = f.read()
 
     local_version = LooseVersion(config['version'])
     print(f"current repo version: {local_version}")
@@ -44,41 +50,51 @@ def update_tailscale():
     print(f"current upstream version: {remote_version}")
 
     if remote_version > local_version:
-        # set build version 
-        build["args"]["TAILSCALE_VERSION"] = remote_version
+        # Pad new config version string to 4 parts
+        config_version = str(remote_version)
+        config_version += ".0" * (3 - config_version.count("."))
 
+        print(f"upgrade required, new version: {config_version}")
+        
+        # set build version 
+        build["args"]["TAILSCALE_VERSION"] = str(remote_version)
+
+        # update build.json
         with open(build_path, "w") as f:
             f.write(json.dumps(build, indent=2))
 
-        # Pad new config version string to 4 parts
-        new_version = str(remote_version)
-        new_version += ".0" * (3 - new_version.count("."))
-
-        print(f"upgrade required, new version: {new_version}")
-        config['version'] = new_version
+        # update config.json
+        config['version'] = config_version
         with open(config_path, "w") as f:
             f.write(json.dumps(config, indent=2))
-        
-        # git add/commit/push
-        call_all_or_fail(
-            ["git", "add", config_path],
-            ["git" "add", build_path],
-            ["git", "commit", "-m", f"updated tailscale to {new_version}"]
-        )
 
-        return (local_version, new_version)
+        # update Dockerfile
+        new_docker = re.sub('^ARG TAILSCALE_VERSION=.*$', 
+            f"ARG TAILSCALE_VERSION=\"{remote_version}\"", 
+            docker, flags=re.MULTILINE)
+
+        with open(docker_path, "w") as f:
+            f.write(new_docker)        
+
+        # git add/commit/push
+        # call_all_or_fail(
+        #     ["git", "add", config_path],
+        #     ["git" "add", build_path],
+        #     ["git", "commit", "-m", f"updated tailscale to {config_version}"]
+        # )
+
+        return (local_version, config_version)
         
     else:
         print("no upgrade required")
         return None
 
-
 if __name__ == "__main__":
     # git setup
-    call_all_or_fail(
-        ["git", "config", "--local", "user.name", "update.py"],
-        ["git", "config", "--local", "user.email", "update.py@hass-addons.git"]
-    )
+    # call_all_or_fail(
+    #     ["git", "config", "--local", "user.name", "update.py"],
+    #     ["git", "config", "--local", "user.email", "update.py@hass-addons.git"]
+    # )
 
     ts_ver_tpl = update_tailscale()
 
